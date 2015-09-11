359174cdf73362c4650aeb8be799bca86020ba0a
Adam Ki Jensen <adajensen@gmail.com>
2015-09-10 18:39:37 -0600

    CRUD func completed.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,18 +10,34 @@ class ArticlesController < ApplicationController
   end
 
   def new
-    @article =Article.new
+    @article = Article.new
   end
 
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "The article, '#{@article.title}' has been created!"
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "The article, '#{@article.title}' has been deleted!"
     redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "The article, '#{@article.title}' has been updated!"
+    redirect_to article_path(@article)
   end
 end
